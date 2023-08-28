6a3e798175ae96c88ae3a798d15891a7b6f5adf3
Kevin Carter <kj.carter.design@gmail.com>
2023-08-28 05:14:34 -0400

    Create cardStyles.js

---
+++ src/components/styles/cardStyles.js
@@ -0,0 +1,14 @@
+export const cardStyle = {
+  width: '240px',
+  height: '336px',
+  borderRadius: '12px',
+  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+  overflow: 'hidden',
+};
+export const cardFormStyle = {
+  width: '312px',  
+  height: '436.8px',  
+  borderRadius: '15.6px',  
+  boxShadow: '0 5.2px 10.4px rgba(0, 0, 0, 0.1)',  
+  overflow: 'hidden',
+};
