f705a8d8cf7ce724a79a5e3e44a25c2a3db6f823
Kevin Carter <kj.carter.design@gmail.com>
2023-08-28 05:14:24 -0400

    update edit and delete

---
+++ src/components/cards/CharacterCard.jsx
@@ -0,0 +1,90 @@
+// CharacterCard.js
+import React from 'react';
+import {
+	Card,
+	CardContent,
+	Typography,
+
+    Grid,
+} from '@mui/material';
+
+const classColors = {
+	fighter: 'red',
+	wizard: 'blue',
+	warlock: 'purple',
+	druid: 'green',
+	// ... add other classes and their colors
+};
+
+const cardStyle = {
+	width: '240px',
+	height: '336px',
+	borderRadius: '12px',
+	boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+	overflow: 'hidden',
+};
+
+function CharacterCard({ character, onDelete, onEdit, mode }) {
+	const borderColor = classColors[character.class] || 'gray';
+
+
+    return (
+        <>
+            <Card
+                style={{
+                    ...cardStyle,
+                    borderColor: borderColor,
+                    borderWidth: '4px',
+                    borderStyle: 'solid',
+                }}
+                onClick={() => onEdit(character)}> 
+                <CardContent>
+                    <Grid container spacing={2}>
+                        {/* Name and Class */}
+                        <Grid item xs={6}>
+                            <Typography variant='h6'>{character.name}</Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Typography variant='subtitle1'>{character.class}</Typography>
+                        </Grid>
+
+                        {/* Image */}
+                        <Grid item xs={12}>
+                            <img
+                                src={character.imageURL}
+                                alt={character.name}
+                                style={{ width: '100%', height: 'auto' }}
+                            />
+                        </Grid>
+
+                        {/* Armor and Enchanted */}
+                        <Grid item xs={6}>
+                            <Typography variant='body2'>Armor: {character.armor}</Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Typography variant='body2'>Magic Item: {character.enchantedItem}</Typography>
+                        </Grid>
+
+                        {/* Main Hand and Off Hand */}
+                        <Grid item xs={6}>
+                            <Typography variant='body2'>Main Hand: {character.mainHand}</Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Typography variant='body2'>Off Hand: {character.offHand}</Typography>
+                        </Grid>
+
+                        {/* Actions and Bonus Actions */}
+                        <Grid item xs={12}>
+                            <Typography variant='body2'>Actions: {character.actions}</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant='body2'>Bonus Actions: {character.bonusActions}</Typography>
+                        </Grid>
+                    </Grid>
+                </CardContent>
+            </Card>
+        </>
+    );
+}
+
+export default CharacterCard;
---
+++ src/helpers/formMethods.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+
+const apiURL = process.env.REACT_APP_API_URL;
+
+
+export const handleEditCharacter = (character, setSelectedCharacter, setFormMode) => {
+    setSelectedCharacter(character);
+};
+
+export const handleCreateCharacter = (setSelectedCharacter, setFormMode) => {
+    setSelectedCharacter(null);
+};
+
+export const handleSubmitCharacter = async (mode, characterData, userID, selectedImage, token, onSubmit) => {
+    const requestData = {
+        ...characterData,
+        userId: userID,
+        image: selectedImage
+    };
+
+    try {
+      if (mode === 'edit' && characterData._id) {
+            // Update existing character
+            const response = await axios.put(`${apiURL}/api/characters/${characterData._id}`, requestData, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            console.log('Character updated:', response.data);
+        } else {
+            // Create new character
+            const response = await axios.post(`${apiURL}/api/characters`, requestData, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            console.log('Character created:', response.data);
+        }
+        onSubmit();
+    } catch (error) {
+        console.error('Error:', error);
+        if (error.response && error.response.status === 401) {
+            alert('Unauthorized: Please log in.');
+        }
+    }
+};
+
+export const deleteCharacter = async (characterId, token) => {
+  try {
+      await axios.delete(`${apiURL}/api/characters/${characterId}`, {
+          headers: {
+              Authorization: `Bearer ${token}`,
+          },
+      });
+  } catch (error) {
+      console.error('Error deleting character:', error);
+  }
+};
